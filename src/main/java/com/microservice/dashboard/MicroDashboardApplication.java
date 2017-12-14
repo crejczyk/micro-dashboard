11577a76f971ea516fa5a07defcff9d16d9f1cbd
ponte <ponte@CAP-CEZARY>
2017-12-14 14:38:50 +0100

    TEST

--- src/main/java/com/microservice/dashboard/MicroDashboardApplication.java
+++ src/main/java/com/microservice/dashboard/MicroDashboardApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroDashboardApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(MicroDashboardApplication.class, args);
+		  SpringApplication.run(MicroDashboardApplication.class, args);
 	}
 }
