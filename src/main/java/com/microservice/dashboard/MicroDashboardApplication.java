4d0ba3ade07b2ff5a2b012dc458c06048c378390
ponte <ponte@CAP-CEZARY>
2017-12-14 14:32:36 +0100

    Init

---
+++ src/main/java/com/microservice/dashboard/MicroDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroDashboardApplication.class, args);
+	}
+}
